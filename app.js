3f913ec915ed9ecaecbc0daf027f45222e36c91e
André Pimentel <and.fvp@gmail.com>
2024-04-15 13:07:53 -0300

    Quinta Versão

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+SERVER_URL = "https://flask-production-81a2.up.railway.app/";
+
 function handleLogin(response) {
     localStorage.setItem("token", response.credential);
+}
+
+function onLinkGoogleAds() {
+    window.location.href = `${SERVER_URL}/authorize`;
 }
