fbd74f88cf7d4e7f698c0b8fa2e6a66233747461
André Pimentel <and.fvp@gmail.com>
2024-04-15 13:29:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-SERVER_URL = "https://flask-production-81a2.up.railway.app/";
+SERVER_URL = "https://flask-production-81a2.up.railway.app";
 
 function handleLogin(response) {
     localStorage.setItem("token", response.credential);
