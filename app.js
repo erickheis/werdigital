f8dd1d9714098d754bb71bf897625c3b400d3309
Pamela de Souza Bella Guerra <40528996+pamsbg@users.noreply.github.com>
2024-05-05 21:00:52 -0300

    adding console.log

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 SERVER_URL = "https://flask-production-81a2.up.railway.app";
 
 function handleLogin(response) {
-    print("response do login")
-    print(response)
+    console.log("response from login:", response);
     localStorage.setItem("token", response.credential);
 }
 
 function onLinkAdsAcount() {
-    token = localStorage.getItem("token")
-    window.location.href = `${SERVER_URL}/authorize?token=${token}`;
+    //token = localStorage.getItem("token")
+    //window.location.href = `${SERVER_URL}/authorize?token=${token}`;
+
+    const token = localStorage.getItem("token");
+    if (token) {
+        window.location.href = `${SERVER_URL}/authorize?token=${encodeURIComponent(token)}`;
+    } else {
+        console.error("Token not found in localStorage.");
+    }
 }
